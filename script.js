bec00c22a996e965ab2836a363182f69fa62187f
aiden456789 <aidena78@nycstudents.net>
2026-06-01 13:33:48 +0000

    update

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let data, mapObj;
 
-async function init(){
-  let link = "violation.json"
-  info = await fetch(link);
+async function init() {
+  let link = "violation.json";
+  let info = await fetch(link);
   data = await info.json();
-  
-  let cards_output = get("cards_output");
+
+  let cards_output = document.getElementById("cards_output");
+
   let build = "";
-  for(let i = 0; i < data.length; i+=1) {
+  for (let i = 0; i < data.length; i++) {
     let violation = data[i];
     build += card(violation);
   }
+
   cards_output.innerHTML = build;
+}
+
+function card(violation) {
+  return `
+    <div class="card fitted">
+      <h3>${violation.borough}</h3>
+      <hr>
+      <p>${violation.housenumber}</p>
+      <p>${violation.streetname}</p>
+      <p>${violation.zip}</p>
+      <p>${violation.currentstatus}</p>
+    </div>
+  `;
 }
